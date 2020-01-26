968341caa8644dd728504b5f31ff7141470d7ff9
Rishi Gargi <gargi.rishi@gmail.com>
2020-01-26 23:36:39 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,17 @@
+curl -sSL https://get.docker.com | sh
+echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json;
+sudo service docker restart
+mkdir /app
+mkdir /app/data
+docker pull koenkk/zigbee2mqtt --platform linux/arm/v6
+docker run -it -v $(pwd)/data:/app/data --network="host" --restart=always --device=/dev/ttyACM0 -e TZ=Asia/Calcutta -v /run/udev:/run/udev:ro --privileged=true koenkk/zigbee2mqtt
+mkdir /opt/offline
+cd /opt/offline
+wget https://raw.githubusercontent.com/richacker/offline-uploader/master/index.js
+wget https://raw.githubusercontent.com/richacker/offline-uploader/master/cred.csv
+wget https://github.com/richacker/offline-uploader/blob/master/config.js
+wget https://raw.githubusercontent.com/richacker/offline-uploader/master/cron.js
+npm i readline
+npm i is-online
+npm i mqtt
+npm i -g forever
