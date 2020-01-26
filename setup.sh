5b78147342b103c2e68f4258c07a33475a54c037
Rishi Gargi <gargi.rishi@gmail.com>
2020-01-27 00:22:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
+echo "starting setup"
 curl -sSL https://get.docker.com | sh
 echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json;
 sudo service docker restart
 mkdir /app
 mkdir /app/data
 docker pull koenkk/zigbee2mqtt --platform linux/arm/v6
-docker run -it -v $(pwd)/data:/app/data --network="host" --restart=always --device=/dev/ttyACM0 -e TZ=Asia/Calcutta -v /run/udev:/run/udev:ro --privileged=true koenkk/zigbee2mqtt
+docker run -it -v $(pwd)/data:/app/data --network="host" --restart=always --device=/dev/ttyACM0 -e TZ=Asia/Calcutta -v /run/udev:/run/udev:ro --privileged=true koenkk/zigbee2mqtt &
 mkdir /opt/offline
 cd /opt/offline
 wget https://raw.githubusercontent.com/richacker/offline-uploader/master/index.js
@@ -15,3 +16,4 @@ npm i readline
 npm i is-online
 npm i mqtt
 npm i -g forever
+echo "setup complete, now run next command: node /opt/offline/config.js <kit number>"
