22c97faf7584bc0bcc81ef44f778942196b2ebd9
Rishi Gargi <gargi.rishi@gmail.com>
2020-02-01 16:10:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 function get_line(filename, line_no, callback) {
     var data = fs.readFileSync(filename, 'utf8');
     var lines = data.split("\n");
-
+    line_no -= 1;
     if(+line_no > lines.length){
       throw new Error('File end reached without finding line');
     }
