7a50a4c600b5e928e15fe1d7b9ff83b93598ffa8
Rishi Gargi <gargi.rishi@gmail.com>
2020-03-18 13:10:08 +0530

    Create vanilla.js

---
+++ vanilla.js
@@ -0,0 +1,47 @@
+
+var fs = require('fs');
+
+try {
+
+    var credfile = '/opt/offline/cred.json';
+    var dockerfile = '/opt/offline/data/configuration.yaml';
+
+
+    fs.readFile(credfile, { encoding: 'utf-8' }, function (err, creddata) {
+        var JSONCredData = JSON.parse(creddata);
+
+
+
+        var newContent = `# Home Assistant integration (MQTT discovery)
+homeassistant: false
+
+# allow new devices to join
+permit_join: true
+
+# MQTT settings
+mqtt:
+  # MQTT base topic for zigbee2mqtt MQTT messages
+  base_topic: ${JSONCredData.topicPrefix}
+  # MQTT server URL
+  server: 'mqtt://mqtt.tfy.ai'
+  # MQTT server authentication, uncomment if required:
+   user: ${JSONCredData.username}
+   password: ${JSONCredData.password}
+
+# Serial settings
+serial:
+  # Location of CC2531 USB sniffer
+  port: /dev/ttyACM0`
+        var newData = newContent + creddata.substring(creddata.lastIndexOf("devices:"), data.length)
+
+        fs.writeFile(dockerfile, newData, function (err) {
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+} catch (e) {
+    console.log(e);
+}
+
+
